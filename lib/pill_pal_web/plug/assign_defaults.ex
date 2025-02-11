5f139e30f9216e359f7b1a8a38302ac63edd3553
Kyi Htoo Aung <kyihtooag@gmail.com>
2025-02-12 03:27:37 +0700

    assign current user to socket
    Signed-off-by: Kyi Htoo Aung <kyihtooag@gmail.com>

--- lib/pill_pal_web/plug/assign_defaults.ex
+++ lib/pill_pal_web/plug/assign_defaults.ex
@@ -1,7 +1,7 @@
 defmodule PillPalWeb.Plug.AssignsDefaults do
   use PillPalWeb, :live_view
 
-  def on_mount(:default, _params, _session, socket) do
-    {:cont, socket}
+  def on_mount(:default, _params, %{"current_user" => current_user} = _session, socket) do
+    {:cont, assign(socket, current_user: current_user)}
   end
 end
